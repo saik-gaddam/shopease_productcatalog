fb96dbb04465fd5378b96cdcb89ef868d381ab80
Sai Krishna Gaddam <chintugaddam1998@gmail.com>
2026-07-05 15:53:04 -0500

    Refactor cart item management to use cartKey

--- cart.js
+++ cart.js
@@ -11,29 +11,31 @@ const Cart = (() => {
     getCount: () => items.reduce((acc, item) => acc + item.quantity, 0),
     getTotal: () => items.reduce((acc, item) => acc + (item.price * item.quantity), 0),
     
-    addItem: (product) => {
-      const match = items.find(i => i.id === product.id);
+    addItem: (product, size = null) => {
+      const cartKey = size ? `${product.id}_${size}` : `${product.id}`;
+      const match = items.find(i => i.cartKey === cartKey);
+      
       if (match) {
         match.quantity += 1;
       } else {
-        items.push({ ...product, quantity: 1 });
+        items.push({ ...product, quantity: 1, selectedSize: size, cartKey: cartKey });
       }
       saveAndDispatch();
     },
     
-    updateQuantity: (id, change) => {
-      const match = items.find(i => i.id === id);
+    updateQuantity: (cartKey, change) => {
+      const match = items.find(i => i.cartKey === cartKey);
       if (match) {
         match.quantity += change;
         if (match.quantity <= 0) {
-          items = items.filter(i => i.id !== id);
+          items = items.filter(i => i.cartKey !== cartKey);
         }
         saveAndDispatch();
       }
     },
     
-    removeItem: (id) => {
-      items = items.filter(i => i.id !== id);
+    removeItem: (cartKey) => {
+      items = items.filter(i => i.cartKey !== cartKey);
       saveAndDispatch();
     },
     
